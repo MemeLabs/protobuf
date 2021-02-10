cc75d13b034c417d90ecd5092b35232838d4bea8
jbpratt78 <jbpratt78@gmail.com>
2021-02-10 16:55:04 -0600

    add gitattributes and ensure test code is deleted

--- android/src/main/java/gg/memelabs/protobuf/bridge/AndroidBridge.kt
+++
@@ -1,8 +0,0 @@
-package org.memelabs.protobuf.bridge
-
-interface AndroidBridge {
-    var onData: (b: ByteArray?) -> Unit
-    fun write(b: ByteArray)
-    fun emitError(msg: String?)
-    fun emitData(b: ByteArray?)
-}
---
+++ hack/delete.bash
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+rm -rf android/src/main/java/gg/
