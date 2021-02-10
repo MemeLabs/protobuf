6c72e52426bfe69331ef96b14efe350df1dbe98b
jbpratt78 <jbpratt78@gmail.com>
2021-02-10 14:34:23 -0600

    rpc for android

---
+++ android/rpc/app/src/main/java/gg/memelabs/protobuf/bridge/AndroidBridge.kt
@@ -0,0 +1,8 @@
+package gg.memelabs.protobuf.bridge
+
+interface AndroidBridge {
+    var onData: (b: ByteArray?) -> Unit
+    fun write(b: ByteArray)
+    fun emitError(msg: String?)
+    fun emitData(b: ByteArray?)
+}
